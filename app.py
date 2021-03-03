072fed9677f576f42a48a7889d273e3a42b3f742
MakeRafa <rafavazquez229@gmail.com>
2021-03-02 17:30:28 -0800

    completed all just need to add sunset comparison

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ from geopy.geocoders import Nominatim
 
 
 ################################################################################
-## SETUP
+# SETUP
 ################################################################################
 
 app = Flask(__name__)
@@ -24,7 +24,7 @@ API_URL = 'http://api.openweathermap.org/data/2.5/weather'
 
 
 ################################################################################
-## ROUTES
+# ROUTES
 ################################################################################
 
 @app.route('/')
@@ -36,45 +36,46 @@ def home():
     }
     return render_template('home.html', **context)
 
+
 def get_letter_for_units(units):
     """Returns a shorthand letter for the given units."""
     return 'F' if units == 'imperial' else 'C' if units == 'metric' else 'K'
+
 
 @app.route('/results')
 def results():
     """Displays results for current weather conditions."""
     # TODO: Use 'request.args' to retrieve the city & units from the query
     # parameters.
-    city = ''
-    units = ''
+    city = request.args.get('city')
+    units = request.args.get('units')
 
     params = {
         # TODO: Enter query parameters here for the 'appid' (your api key),
+        'appid': API_KEY,
         # the city, and the units (metric or imperial).
+        'q': city,
+        'units': units
         # See the documentation here: https://openweathermap.org/current
-
     }
-
     result_json = requests.get(API_URL, params=params).json()
-
     # Uncomment the line below to see the results of the API call!
-    # pp.pprint(result_json)
-
+    pp.pprint(result_json)
     # TODO: Replace the empty variables below with their appropriate values.
     # You'll need to retrieve these from the result_json object above.
 
     # For the sunrise & sunset variables, I would recommend to turn them into
-    # datetime objects. You can do so using the `datetime.fromtimestamp()` 
+    # datetime objects. You can do so using the `datetime.fromtimestamp()`
     # function.
     context = {
         'date': datetime.now(),
-        'city': '',
-        'description': '',
-        'temp': '',
-        'humidity': '',
-        'wind_speed': '',
-        'sunrise': '',
-        'sunset': '',
+        'city': city,
+        'description': result_json['weather'][0]['description'],
+        'temp': result_json['main']['temp'],
+        'humidity': result_json['main']['humidity'],
+        'wind_speed': result_json['wind']['speed'],
+        'sunrise': datetime.fromtimestamp(result_json['sys']['sunrise']),
+        'sunset': datetime.fromtimestamp(result_json['sys']['sunset']),
         'units_letter': get_letter_for_units(units)
     }
 
@@ -86,22 +87,51 @@ def comparison_results():
     """Displays the relative weather for 2 different cities."""
     # TODO: Use 'request.args' to retrieve the cities & units from the query
     # parameters.
-    city1 = ''
-    city2 = ''
-    units = ''
+    city1 = request.args.get('city1')
+    city2 = request.args.get('city2')
+    units = request.args.get('units')
 
-    # TODO: Make 2 API calls, one for each city. HINT: You may want to write a 
+    # TODO: Make 2 API calls, one for each city. HINT: You may want to write a
     # helper function for this!
 
+    def get_results(city):
+        params = {
+            # TODO: Enter query parameters here for the 'appid' (your api key),
+            'appid': API_KEY,
+            # the city, and the units (metric or imperial).
+            'q': city,
+            'units': units
+            # See the documentation here: https://openweathermap.org/current
+        }
+        return requests.get(API_URL, params=params).json()
 
     # TODO: Pass the information for both cities in the context. Make sure to
     # pass info for the temperature, humidity, wind speed, and sunset time!
-    # HINT: It may be useful to create 2 new dictionaries, `city1_info` and 
+    # HINT: It may be useful to create 2 new dictionaries, `city1_info` and
     # `city2_info`, to organize the data.
+    city1_json = get_results(city1)
+    city2_json = get_results(city2)
+
     context = {
-
+        'date': datetime.now(),
+        'units_letter': get_letter_for_units(units),
+        'city1_info': {
+            'city1': city1,
+            'temp': city1_json['main']['temp'],
+            'humidity': city1_json['main']['humidity'],
+            'wind_speed': city1_json['wind']['speed'],
+            'sunrise': datetime.fromtimestamp(city1_json['sys']['sunrise']),
+            'sunset': datetime.fromtimestamp(city1_json['sys']['sunset']),
+        },
+        'city2_info': {
+            'city2': city2,
+            'temp': city2_json['main']['temp'],
+            'humidity': city2_json['main']['humidity'],
+            'wind_speed': city2_json['wind']['speed'],
+            'sunrise': datetime.fromtimestamp(city2_json['sys']['sunrise']),
+            'sunset': datetime.fromtimestamp(city2_json['sys']['sunset']),
+        }
     }
-
     return render_template('comparison_results.html', **context)
 
 
